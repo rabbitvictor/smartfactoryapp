7ca369cff12476321d871b356a3407690c79cb88
Víctor Coelho <victor.coelho@codex.com.br>
2022-09-14 23:51:48 -0300

    Adding Scaffold

--- app/src/main/java/com/mc855/app/MainActivity.kt
+++ app/src/main/java/com/mc855/app/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -20,19 +23,29 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.Divider
+import androidx.compose.material.DrawerValue
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.ListItem
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.SnackbarResult
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Lens
 import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material.rememberDrawerState
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -42,21 +55,70 @@ import androidx.compose.ui.unit.dp
 import com.mc855.app.network.model.UserEntity
 import com.mc855.app.network.model.getUsers
 import com.mc855.app.ui.theme.SmartFactoryAppTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
 			SmartFactoryAppTheme {
-				UsersList()
+				Scaffold()
 			}
+		}
+	}
+
+	@Composable
+	private fun Scaffold() {
+		val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
+		val coroutineScope = rememberCoroutineScope()
+		Scaffold(
+			scaffoldState = scaffoldState,
+			topBar = { TopAppBar(coroutineScope, scaffoldState) },
+			drawerContent = { Drawer() },
+			floatingActionButton = {
+				FloatingActionButton(coroutineScope, scaffoldState)
+			}
+		) { contentPadding ->
+			UsersList(contentPadding)
+		}
+	}
+
+	@Composable
+	private fun FloatingActionButton(
+		coroutineScope: CoroutineScope,
+		scaffoldState: ScaffoldState
+	) {
+		FloatingActionButton(
+			onClick = {
+				coroutineScope.launch {
+					when (scaffoldState.snackbarHostState.showSnackbar(
+						message = "Snack Bar",
+						actionLabel = "Dismiss"
+					)) {
+						SnackbarResult.ActionPerformed -> TODO()
+						SnackbarResult.Dismissed -> TODO()
+					}
+				}
+			},
+		) {
+			Icon(Icons.Filled.Add, "")
+		}
+	}
+
+	@Composable
+	private fun Drawer() {
+		Text("Drawer", modifier = Modifier.padding(16.dp))
+		Divider()
+		Row() {
+
 		}
 	}
 }
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun UsersList() {
+fun UsersList(contentPadding: PaddingValues) {
 	Log.d("xapp-called", "Compose")
 
 	val users = produceState(
@@ -66,11 +128,10 @@ fun UsersList() {
 			Log.d("xapp-called", "Producer")
 		}
 	)
-
 	LazyColumn(
 		Modifier.fillMaxSize(),
-		contentPadding = PaddingValues(0.dp),
-		verticalArrangement = Arrangement.spacedBy(0.dp)
+		contentPadding = contentPadding,
+		verticalArrangement = Arrangement.spacedBy(10.dp)
 	) {
 		items(users.value) { user ->
 			Surface(
@@ -81,7 +142,7 @@ fun UsersList() {
 						Text(user.title)
 					},
 					secondaryText = {
-						Text(user.id )
+						Text(user.id)
 					},
 					icon = {
 						Icon(
@@ -92,11 +153,23 @@ fun UsersList() {
 						)
 					}
 				)
-				Divider(thickness = 5.dp)
 			}
 		}
 	}
 }
+
+@Composable
+fun TopAppBar(scope: CoroutineScope, scaffoldState: ScaffoldState) {
+	TopAppBar(
+		title = { Text(text = "Lista de Sensores") },
+		modifier = Modifier.clickable {
+			scope.launch {
+				scaffoldState.drawerState.open()
+			}
+		}
+	)
+}
+
 
 @Composable
 private fun chooseColor(hasCompleted: Boolean) =
